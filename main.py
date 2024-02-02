faf62c9fd5c9b01a698ad7e792f8a572ce70eb8d
MuhammadTayyabCoder <muhammadtayyabf21@gmail.com>
2024-02-02 21:17:10 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the First Number: "))
+b = int(input("Enter the Second Number: "))
+if a > b:
+    print(f"{a} is greater than {b}")
+elif b > a:
+    print(f"{b} is greater than {a}")
+else:
+    print("Both numbers are equal")
